873ad9268ef4219e5dc1cebfac4fc033297fbe51
Vstreber <vstreber@smith.edu>
2025-09-27 18:42:19 -0400

    Random chess

--- RandomChess.py
+++ RandomChess.py
@@ -1,5 +1,6 @@
 import chess
-
+import random
+import chess.svg
 
 print("=====================================================\nCS 290 Chess Bot Version 0.1 \n=====================================================")
 
@@ -8,3 +9,7 @@ print(board)
 
 board.push_uci("e2e4")
 print(board)
+
+
+
+print(board.legal_moves)
--- chessbot.py
+++ chessbot.py
@@ -1,35 +1,61 @@
 import datetime
 import chess
+import random
+import re
+
+board = chess.Board()
+
+now = datetime.datetime.now()
+print(now)
 
 print("Computer Player? (w=white/b=black): ")
 color = input()
-now = datetime.datetime.now()
-print(now)
-if(color == "w"):
-    computer_color = "b"
-else:
-    computer_color = "w"
+x = True
 
+while x:
+    if(color == "w"):
+        computer = "b"
+        x = False
+    elif(color == "b"):
+        computer = "w"
+        x = False
+    else:
+        print("Please enter 'w' or 'b'")
+        color = input()
+
+current_player = "w"
+human = color
+
+print("Please enter your starting position.")
 starting_position = input()
-current_player = color
 if(starting_position == ""):
     starting_position = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR"
-if color == "w":
-    move = input()
-else:
-    # bot command
-    print("x") #delete later
 
-while True:
-    if current_player == "w" & current_player == color:
+board.set_fen(starting_position)
+
+print(board)
+
+while (board.is_checkmate() == False):
+    if human == current_player:
+        print("Enter your move: ")
         move = input()
-        current_player == "b"
-    if current_player == "w" & current_player != color:
-        # bot command
-        current_player == "b"
-    if current_player == "b" & current_player == color:
-        move = input()
-        current_player == "w"
-    if current_player == "b" & current_player != color:
-        # bot command
-        current_player == "w"
+
+        # while move not in list(board.legal_moves):
+        #     move = input("Please input a valid move. ")
+
+        board.push_uci(move)
+        current_player = "b"
+    elif computer == current_player:
+        print("The bot's move is: ")
+        legal_moves = list(board.legal_moves)
+        move = random.choice(legal_moves)
+        move = str(move)
+        print(move)
+        board.push_uci(move)
+        current_player = "w"
+
+    print("New Fen Position" + board.fen())
+    print(board)
+
+    if board.is_checkmate() == True:
+        print("Checkmate!")
