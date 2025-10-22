6f3909e0fda656b61212ca7139444204c12ed978
Vstreber <vstreber@smith.edu>
2025-10-22 11:54:05 -0400

    updated minimax

--- Chess Bot/Minimax Bot/test.py
+++
@@ -1,9 +0,0 @@
-def recursive_function(n, depth=0, max_depth=10):
-    ## Prevent excessive recursion
-    if depth >= max_depth:
-        return None
-
-    ## Recursive logic
-    if n > 0:
-        return recursive_function(n - 1, depth + 1, max_depth)
-    return n
